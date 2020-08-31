d2bd3bbd50430cc2bda8d4b0c0ed32b179a68a2e
Sean Wu <seanwu1105@gmail.com>
2020-08-31 23:43:02 +0800

    Add alternative solution.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Capacitor, FilesystemDirectory, FilesystemEncoding, Plugins } from '@capacitor/core';
+import { Capacitor, FilesystemDirectory, Plugins } from '@capacitor/core';
 
 const { Filesystem } = Plugins;
 
@@ -11,17 +11,22 @@ const { Filesystem } = Plugins;
 export class HomePage implements OnInit {
 
   src = '';
+  base64Src = '';
 
   constructor() { }
 
   async ngOnInit() {
-    const result = await Filesystem.writeFile({
+    const writeResult = await Filesystem.writeFile({
       data: 'iVBORw0KGgoAAAANSUhEUgAAAAYAAAAECAYAAACtBE5DAAAABHNCSVQICAgIfAhkiAAAABl0RVh0U29mdHdhcmUAZ25vbWUtc2NyZWVuc2hvdO8Dvz4AAAA9aVRYdENyZWF0aW9uIFRpbWUAAAAAADIwMjDlubTlhavmnIgzMeaXpSAo6YCx5LiAKSAyM+aZgjI35YiGMjTnp5IACi9UAAAASElEQVQImWOUk5P7z4AFsEAoSQa/kGYGu2ezGbazuzP4K8jBJBgYGBjYGETVwxiszi9gaDp0A1mCgYHh7U6G6RcuM7xhYGAAAEFTDx/VUsAUAAAAAElFTkSuQmCC',
       directory: FilesystemDirectory.Data,
       path: 'demo',
-      recursive: true,
-      encoding: FilesystemEncoding.UTF8
+      recursive: true
     });
-    this.src = Capacitor.convertFileSrc(result.uri);
+    this.src = Capacitor.convertFileSrc(writeResult.uri);
+    const readResult = await Filesystem.readFile({
+      directory: FilesystemDirectory.Data,
+      path: 'demo'
+    });
+    this.base64Src = `data:image/png;base64,${readResult.data}`;
   }
 }
